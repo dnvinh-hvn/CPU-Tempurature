f8425e4414b947bf038eac79e9d72623d7fa7874
dnvinh <dnvinh@humaxdigital.com>
2021-01-29 15:03:07 +0700

    Update function get cpu tempurature for box

--- app/src/main/java/com/vinhdn/cputemp/cpu/CpuTools.java
+++ app/src/main/java/com/vinhdn/cputemp/cpu/CpuTools.java
@@ -1,5 +1,9 @@
 package com.vinhdn.cputemp.cpu;
 
+import android.content.Context;
+import android.os.HardwarePropertiesManager;
+
+import com.vinhdn.cputemp.App;
 import com.vinhdn.cputemp.tool.IOTools;
 
 import java.io.BufferedReader;
@@ -9,6 +13,24 @@ public class CpuTools {
     private static final String CPU_TEMP = "cat sys/class/thermal/thermal_zone0/temp";
 
     public static int getTemp() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            Object objService = App.instance.getSystemService(Context.HARDWARE_PROPERTIES_SERVICE);
+            if(objService instanceof HardwarePropertiesManager) {
+                HardwarePropertiesManager hardware = (HardwarePropertiesManager) objService;
+                float[] cpuTemperature;
+                try {
+                    cpuTemperature = hardware.getDeviceTemperatures(HardwarePropertiesManager.DEVICE_TEMPERATURE_CPU, HardwarePropertiesManager.TEMPERATURE_CURRENT);
+                    float total = 0;
+                    for (float v : cpuTemperature) {
+                        total = total + v;
+                    }
+                    double percentAvail = total / cpuTemperature.length;
+                    return (int) (Math.round(percentAvail * 10) / 10.0);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
         BufferedReader reader = null;
         try {
             Process process = Runtime.getRuntime().exec(CPU_TEMP);
